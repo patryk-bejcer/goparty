dbab97510bfba7eb98d660d780bc50a84a73e5ea
mateusztumatek <37153688+mateusztumatek@users.noreply.github.com>
2018-05-19 15:55:47 +0200

    Home Page, Search Bar, Single club (events in single club doesn't work)

--- resources/views/dashboard/clubs/single.blade.php
+++ resources/views/dashboard/clubs/single.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <div class="container" style="max-width: 85%; padding: 0px; margin-top: 120px;" id="single_club" >
+    <div class="container" style="max-width: 85%; padding: 0px; margin-top: 120px;" id="single_club" data-scroll = 'scroll'>
 
         <div class="row justify-content-center">
 
@@ -47,7 +47,7 @@
             @php var_dump($rules) @endphp
             @elseif(!empty($rules))
 
-        @
+
         <h3 class="text-center mb-2 mt-2">Zasady: </h3>
         <div class="row justify-content-center" style="margin-top: 30px; padding: 0px;">
             @foreach($rules as $rule)
@@ -63,16 +63,14 @@
 
         </div>
         @endif
-        @php
-            $club2 = \App\Models\Club::findOrFail(4);
-        @endphp
+
         <h3 class="text-center mt-5 mb-5">Kluby w pobliżu</h3>
     </div>
         <div class="row justify-content-center" style="padding:0px; max-width: 90%; margin: auto;  position: relative; margin-top: 80px; position: relative; height: 550px">
             <a class="btn-prev text-left align-left"> Poprzedni </a>
             @php $count = 0; @endphp
             @foreach($club->getClosestClubs($club) as $key => $my_club)
-                @php $closest_club = \App\Models\Club::findorFail($my_club[0]['id']) @endphp
+
             <div class="club-slide"  @if($count == 2) id="club-slide-left" @elseif($count == 1) id="club-slide-right" @elseif($count == 0) id = 'club-slide-center' @endif style="margin: auto; margin-bottom: 50px;">
                 <img id="box-shadow" src="{{url('img/box-shadow.png')}}">
                 @if(empty(\App\Models\Club::getMain($my_club[0]['id'])))
@@ -119,9 +117,82 @@
                     @break
                 @endif
                 @endforeach
-            <a  id="left" class="navigator" onclick="prev_slide()" ><i class="fa fa-angle-left"></i> </a>
-            <a  id="right" class="navigator" onclick="next_slide()" >  <i class="fa fa-angle-right"></i></a>
+            <a  style="position: absolute; top: 33%" id="left" class="navigator" onclick="prev_slide()" ><i class="fa fa-angle-left"></i> </a>
+            <a   style="position: absolute; top: 33%" id="right" class="navigator" onclick="next_slide()" >  <i class="fa fa-angle-right"></i></a>
+
+
+
         </div>
+        <div class="container" id="events">
+            <div class="row justify-content-center">
+                <div class="col-lg-12 text-center">
+                    <h3>Wydarzenia w tym klubie: </h3>
+                </div>
+                <div id="event-carousel" class="carousel carousel-events slide carousel-fade" data-ride="carousel" >
+                    <div class="carousel-inner">
+                        @php
+                            $counter = 0;
+                            $site = 1;
+                        @endphp
+                        @foreach($events as $event)
+
+                            @php $counter++; @endphp
+                            <div id="shadow-outside" class="carousel-item @if($counter == 1) active @endif " style="height: 420px; overflow: hidden">
+                                <div class="event-image">
+
+                                    <img class="event-image" src="{{asset('img/klub1.jpg')}}">
+                                    <div id="shadow-inside"> </div>
+
+                                </div>
+
+                                        <div class="col-lg-12 event" style="padding-top:1rem; padding-bottom: 1rem"   >
+
+                                            <div class="row" >
+                                                <div class="col-lg-1" >
+                                                    <h1 style=" margin: 0px; @if($counter>9) font-size: 50px !important; margin-top: 35% !important; @endif"> {{$club->id}}. </h1>
+                                                </div>
+                                                <div class="col-lg-11" >
+                                                    <p style="margin-top: 5px;" id="event-name" >{{$event->title}}</p>
+                                                    <p id="event-date" >{{substr($event->start_date,0,10)}} <span class="pull-right">{{rand(200,9000)}} osób</span> </p>
+
+                                                </div>
+                                            </div>
+                                            <hr style="background-color: white; text-align: center; margin-top: 5px">
+                                            <div  id="event-article" class="col-lg-19 text-center " style="; padding: 0px;">
+                                                <article >
+                                                    {{$event->description}}
+                                                </article>
+
+                                            </div>
+                                            <a style=" font-size: 20px;" class="mt-3 myBtn-link myBtn hvr-sweep-to-right animated fadeIn">Zobacz wiecej</a>
+                                        </div>
+
+
+
+
+                            </div>
+                        @endforeach
+
+
+                    </div>
+
+                    <a  class="carousel-control-prev " href="#event-carousel" data-slide="prev" style="font-size:4em; color:black; z-index: 1; left: -11%; top:0px !important;">
+                        <span  class="carousel-control fa fa-caret-left fa-sm" ></span>
+                    </a>
+                    <a class="carousel-control-next " href="#event-carousel" data-slide="next" style="font-size:4em; color:black; z-index: 1; right: -11%; top:0px !important;">
+                        <span  class="carousel-control fa fa-caret-right fa-sm" ></span>
+                    </a>
+
+                </div>
+
+            </div>
+        </div>
+
+            </div>
+
+        </div>
+
+
 
 
 
